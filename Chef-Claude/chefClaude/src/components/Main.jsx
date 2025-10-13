8ddeee23412363eca09df386afb61ff9a90243b6
aditya8755590 <aditya.kumar875590@gmail.com>
2025-10-13 21:33:39 +0530

    [aditya].addItems functionlity

--- Chef-Claude/chefClaude/src/components/Main.jsx
+++ Chef-Claude/chefClaude/src/components/Main.jsx
@@ -1,15 +1,33 @@
+import React from "react"
 export default function Main() {
+  const [ingredients, setIngredients] = React.useState([]);
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+    function handleSubmit(event) {
+        event.preventDefault();
+         const formData = new FormData(event.currentTarget)
+         const newIngredient = formData.get("ingredient")
+        setIngredients([...ingredients, newIngredient]);
+    }
   return (
     <main className="main">
       <h2>Welcome to Chef Claude's Kitchen</h2>
       <p>Discover delicious recipes and cooking tips.</p>
-      <form className="search">
-        <input2
+      <form className="search" onSubmit={handleSubmit}>
+        <input
         type="text" 
         placeholder="eg .tomato" 
+        name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <div className="items">
+         <ul>
+        {ingredientsListItems}
+      </ul>
+      </div>
     </main>
   )
 }
