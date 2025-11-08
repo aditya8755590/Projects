6e2439b771d4f5cced3f4cd7c5efb1119d20dfc5
aditya8755590 <aditya.kumar875590@gmail.com>
2025-11-08 16:01:37 +0530

    finilising up to changing the color propties and also going to complete very soon

---
+++ Tenzies/src/SubMain.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { nanoid } from "nanoid";
+import DieComp from './dieCom'
+export default function SubMain(){
+
+  function generateAllNewDice(){
+    const arr=[]
+    for(let i=0;i<10;i++){
+        arr[i]={value:Math.floor(Math.random()*6)+1
+          ,isHeld:true
+          ,id: nanoid()
+        }
+    }
+    return(
+        arr
+    )
+}
+
+function hold(id){
+  setDieState (DieState=> DieState.map(die => {
+            return die.id===id ? {...die, isHeld: !die.isHeld} :die
+        }))
+}
+
+const [DieState,setDieState] = React.useState(generateAllNewDice);
+
+const diceArr=DieState.map((e)=>{
+   return <DieComp  key={e.id} id={e.id} value={e.value} isHeld={e.isHeld} hold={hold}/>
+})
+
+function suffelDice(){
+  setDieState(generateAllNewDice)
+}
+    return(
+        <section className="our-section">
+        <section className="content">
+
+          <section>
+           <h1>TENIZIES</h1>
+           <p>Roll until all dice are the same. 
+            Click each die to freeze it at its current value between rolls.</p>
+          </section>
+
+          <section className="contianner">
+                <div className="Box-contianner">
+                   {diceArr}
+                </div>
+
+          </section>
+         <div className="butt">
+          <button onClick={suffelDice}>Roll</button>
+         </div>
+        </section>
+        </section>
+    )
+}
