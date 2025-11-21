57cd9da795a5faa1ba7204410f38d56bcb1d4867
aditya8755590 <aditya.kumar875590@gmail.com>
2025-11-21 18:55:22 +0530

    publish

---
+++ build_A_Rest_API/server.js
@@ -0,0 +1,19 @@
+import http from 'node:http';
+const port=3007;
+
+
+const server=http.createServer((req,res)=>{
+   if(req.method==='GET'){
+    res.write('This is some data \n')
+   res.write('This is some more data \n')
+    res.end("hello ji request recive ho gai hai !!")
+   }
+   else{
+    req.statusCode=404
+    res.end("hello koi input nahai hai")
+   }
+})
+
+server.listen(port,()=>{
+    console.log(`Server is listening on port  ${port}....`)
+})
