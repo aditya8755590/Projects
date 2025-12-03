ef945fbe45a92e6f81e842010ebed145eb5c974d
aditya8755590 <aditya.kumar875590@gmail.com>
2025-12-03 17:27:27 +0530

    start a new project

--- Tenzies/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import SubMain from './SubMain.jsx'
-export default function App() {
-  return (
-   <main className="main-box">
-     <SubMain/>
-   </main>
-  )
-}
--- Tenzies/src/dieCom.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function (prop){
-     const styles = {
-        backgroundColor: prop.isHeld ? "#59E391" : "white"
-    }
-    return(
-        <button className="box"  style={styles} onClick={()=>{prop.hold(prop.id)}}>{prop.value}</button>
-    )
-}
