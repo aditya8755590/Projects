8af6329ead2f6398a87b795ad8a93ee81677bfc1
aditya8755590 <aditya.kumar875590@gmail.com>
2025-12-06 19:05:51 +0530

    adding two iframes in user route

--- react-router-project/my-project/src/components/User.jsx
+++ react-router-project/my-project/src/components/User.jsx
@@ -8,7 +8,15 @@ const User = () => {
     const {userid} = useParams()
 
   return (
-    <div>User{userid} Funck YOU</div>
+    
+
+    <div>User {userid}
+     <div className="box"></div>
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/HhzZ_hvyUyA?si=Ubj22mPMjEjCpUIp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/IpFX2vq8HKw?si=EEY12qL62dVu0muP&amp;start=4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    
+    
   )
 
 }
