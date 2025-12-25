952667e8dc105b75c21aa78b769f9facf7575b34
aditya8755590 <aditya.kumar875590@gmail.com>
2025-12-25 21:05:07 +0530

    make another server

--- build_A_Rest_API/server.js
+++ build_A_Rest_API/server.js
@@ -3,15 +3,21 @@ const port=3007;
 
 
 const server=http.createServer((req,res)=>{
-   if(req.method==='GET'){
-    res.write('This is some data \n')
-   res.write('This is some more data \n')
-    res.end("hello ji request recive ho gai hai !!")
-   }
+//    if(req.method==='GET'){
+//     res.write('This is some data \n')
+//    res.write('This is some more data \n')
+//     res.end("hello ji request recive ho gai hai !!")
+//    }
+//    else{
+//     req.statusCode=404
+//     res.end("hello koi input nahai hai")
+//    }
+if(req.url=="/api"&&req.method==='GET'){
+   res.end('today is christmas fuck you ')}
    else{
-    req.statusCode=404
-    res.end("hello koi input nahai hai")
+    res.end('lund lai lo nahi to api dalo bas ');
    }
+
 })
 
 server.listen(port,()=>{
