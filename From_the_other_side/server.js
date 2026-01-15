a14bca31d79006805fcf4950aba7cad642a86598
aditya8755590 <aditya.kumar875590@gmail.com>
2026-01-15 07:36:12 +0530

    final updated to add more routes in server .js

--- From_the_other_side/server.js
+++ From_the_other_side/server.js
@@ -12,10 +12,20 @@ const server = http.createServer(async (req, res) => {
         if (req.method === 'GET') {
             return await handleGet(res)
         }
+          else if (req.method === 'POST') {
+            handlePost(req, res)
+        }
+
+    }
+    else if (req.url === "/api/news") {
+
+      return await handleNews(req, res)
+
     }
     else if (!req.url.startsWith('/api')) {
         return await serveStatic(req, res, __dirname)
     }
+
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
