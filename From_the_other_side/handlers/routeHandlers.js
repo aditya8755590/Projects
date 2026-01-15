238abd9f1cc96db977b3f2f36c095e714d44b707
aditya8755590 <aditya.kumar875590@gmail.com>
2026-01-15 07:53:57 +0530

    correcting the error

--- From_the_other_side/handlers/routeHandlers.js
+++ From_the_other_side/handlers/routeHandlers.js
@@ -1,16 +1,51 @@
 import { getData } from '../utils/getData.js'
 import { sendResponse } from '../utils/sendResponse.js'
+import { parseJSONBody } from '../utils/parseJSONBody.js'
+import { addNewSighting } from '../utils/addNewSighting.js'
+import { sanitizeInput } from '../utils/sanitizeInput.js'
+import { sightingEvents } from '../events/sightingEvents.js'
+import { stories } from "../data/stories.js";
 
-export async function handleGet(res) {
+
+export async function handleGet(res) { 
   const data = await getData()
   const content = JSON.stringify(data)
   sendResponse(res, 200, 'application/json', content)
-}  
+}
 
-// handlePost
+export async function handlePost(req, res) {
 
-/*
-Challenge:
-  1. Create and export a function called handlePost().
-  2. For now, that function can just log 'POST request received'.
-*/
+  try {
+    const parsedBody = await parseJSONBody(req)
+    const sanitizedBody = sanitizeInput(parsedBody)
+    await addNewSighting(sanitizedBody)
+    sightingEvents.emit('sighting-added', sanitizedBody)
+    sendResponse(res, 201, 'application/json', JSON.stringify(sanitizedBody))
+  } catch (err) {
+    sendResponse(res, 400, 'application/json', JSON.stringify({ error: err }))
+  } 
+
+}
+
+export async function handleNews(req, res) {
+  res.statusCode = 200
+
+  res.setHeader("Content-Type", "text/event-stream")
+  res.setHeader("Cache-Control", "no-cache")
+  res.setHeader("Connection", "keep-alive")
+
+  setInterval(() => {
+    let randomIndex = Math.floor(Math.random() * stories.length)
+
+    res.write(
+      `data: ${JSON.stringify({
+        event: 'news-update',
+        story: stories[randomIndex]
+      })}\n\n`
+    )
+
+  }, 3000)
+
+}
+
+
--- From_the_other_side/server.js
+++ From_the_other_side/server.js
@@ -1,6 +1,8 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
 import { handleGet } from './handlers/routeHandlers.js'
+import { handlePost } from './handlers/routeHandlers.js'
+import { handleNews } from './handlers/routeHandlers.js'
 
 const PORT = 8000
 
