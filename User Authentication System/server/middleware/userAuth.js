ab765a65ca44dea10c4b303a78c9e0b0365f38d0
aditya8755590 <aditya.kumar875590@gmail.com>
2026-02-07 16:47:52 +0530

    Refactor authentication routes to include email verification; add userAuth middleware for secure OTP handling

--- User Authentication System/server/middleware/userAuth.js
+++ User Authentication System/server/middleware/userAuth.js
@@ -1,1 +1,26 @@
 import jwt from 'jsonwebtoken';
+
+const userAuth = async (req,res,next)=>{
+
+    const token=req.cookies.token;
+    if(!token){
+        return res.json({success:false,message:"unauthorized"})
+    }
+    try{
+
+        const decoded=jwt.verify(token,process.env.JWT_SECRET);
+        
+        if(decoded.id){
+            req.body.userId=decoded.id;
+        }
+        else{
+            return res.json({success:false,message:"invalid token"})
+        }
+        return next();
+    }
+    catch(error){
+        return res.json({success:false,message:"invalid token"})
+    }
+}
+
+export default userAuth;
--- User Authentication System/server/routes/authRoutes.js
+++ User Authentication System/server/routes/authRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { login,register,logout } from '../controller/authControler.js';
+import { sendVerifyOtp, verifyEmail } from '../controller/authControler.js';
+import userAuth  from '../middleware/userAuth.js';
 const authRouter=express.Router();
 authRouter.post('/register',register);
 authRouter.post('/login',login);
 authRouter.post('/logout',logout);
-export default authRouter
-
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
+authRouter.post('/verify-email', userAuth, verifyEmail);
+export default authRouter;
